b3f938977f446384efc9d3a539e07e0b08810cfc
FaridaElsheikh <faridaelsheikh2000@gmail.com>
2021-04-17 12:40:45 +0300

    final update

---
+++ LoginFunction.js
@@ -0,0 +1,22 @@
+function Login(){
+    var radio=document.getElementsByName('logintype');
+    var value;
+    for(var i of radio) {
+        if(i.checked){
+            value=i.value;
+        }
+    }
+    if(value =='Student'){
+        window.location.replace("./StudentPage.html");
+    }
+    else if(value=='Instructor'){
+        window.location.replace("./SecretaryPage.html");
+    }
+    else if(value=='Secretary') {
+        window.location.replace("./SecretaryPage.html");
+    }
+    
+}
+function redirect(){
+    window.location.replace("./StudentPage.html");
+}
